c27eb1b2780f8a47e38cdc4c97616ace4aeba6c9
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-01-23 20:19:42 -0500

    Remove App.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import QuestionInput from './components/QuestionInput';
 import * as serviceWorker from './serviceWorker';
 import { Card } from 'antd';
